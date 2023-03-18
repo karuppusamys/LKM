14fab4d9aa5aa867ec3155c0961942602fc37afc
Karuppusamy S <karuppusamy.kovai@gmail.com>
2023-03-18 14:24:23 +0530

    Hello World

--- Modules/01_Hello_World/src.c
+++ Modules/01_Hello_World/src.c
@@ -1,2 +1,22 @@
 #include <linux/module.h>
 
+static int __init start_fun(void)
+{
+	printk(KERN_INFO "Hello World\n");
+	return 0;
+}
+
+static void __exit exit_fun(void)
+{
+	printk(KERN_INFO "Good bye\n");
+}
+
+module_init(start_fun);
+module_exit(exit_fun);
+
+
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
+MODULE_AUTHOR("KS");
+MODULE_DESCRIPTION("Hello World");
+
--- main.c
+++
@@ -1,4 +0,0 @@
-tesdfdfd
-:wq
-:wq
-t
